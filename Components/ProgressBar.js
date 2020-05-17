4ac091596d19443db880fc459a8e8a37a94a415d
karmina-jc <karmina.jarac@gmail.com>
2020-05-16 20:43:03 -0400

    estilos de barra de progreso y funcionalidad base de pasos

--- Components/ProgressBar.js
+++ Components/ProgressBar.js
@@ -1,34 +1,55 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-import { MaterialCommunityIcons as Icon} from 'react-native-vector-icons'
-
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 function ProgressBar() {
+
+    const [datos, setDatos] = useState(true)
+    const [validate, setValidate] = useState(false)
+    const [transfer, setTransfer] = useState(false)
+    const [complete, setComplete] = useState(false)
+
+ 
+
     return (
         <View style={styles.fullbar}>
             <View style={styles.step}>
-                <Icon 
+                <Image 
+                source={require('../assets/datos-active.png')}
                 style={styles.stepIcon}
-                name="circle" />
-                <Text>Step 1</Text>
+                />
+                <Text style={styles.description}>
+                    Datos de retiro enviados
+                </Text>
+                <View style={styles.line}></View>
             </View>
             <View style={styles.step}>
-                <Icon 
+                <Image 
+                source={validate ? require('../assets/valid-active.png') : require('../assets/valid-default.png')}
                 style={styles.stepIcon}
-                name="circle" />
-                <Text>Step 2</Text>
+                />
+                <Text style={styles.description}>
+                    Validando tranferencia
+                </Text>
+                <View style={styles.line}></View>
             </View>
             <View style={styles.step}>
-                <Icon 
+                <Image 
+                source={require('../assets/transfer-default.png')}
                 style={styles.stepIcon}
-                name="circle" />
-                <Text>Step 3</Text>
+                />
+                <Text style={styles.description}>
+                    Transfiriendo a tu banco
+                </Text>
+                <View style={styles.line}></View>
             </View>
             <View style={styles.step}>
-                <Icon 
+                <Image 
+                source={require('../assets/done-default.png')}
                 style={styles.stepIcon}
-                name="circle" />
-                <Text>Step 4</Text>
+                />
+                <Text style={styles.description}>
+                    Tranferencia completada
+                </Text>
             </View>            
         </View>
     )
@@ -40,6 +61,26 @@ const styles = StyleSheet.create({
       justifyContent: 'space-around',
       alignItems: 'center',
     },
+    step: {
+        alignItems: 'center',
+        position:'relative',
+    },
+    stepIcon: {
+        width: 50,
+    },
+    description:{
+        fontSize: 10,
+        width:'70%',
+        textAlign: 'center',
+    },
+    line: {
+        height: 1,
+        width:'45%',
+        backgroundColor: '#595959',
+        position: 'absolute',
+        top: '30%',
+        left: '74%',              
+    }
 });
   
 export default ProgressBar;
