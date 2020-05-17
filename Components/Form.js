b7e012cb311555f3f1ce9677682070da7ef00f88
consuelowevar <consue_1990@hotmail.com>
2020-05-17 13:41:27 -0400

    caja de preguntas estilos OK

--- Components/Form.js
+++ Components/Form.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Modal, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Button, Separator } from 'react-native';
 import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons'
 
 
@@ -24,29 +24,49 @@ export default function Form() {
 
         </Modal>
 
-        <View style={styles.question}>
-          <TouchableOpacity style={styles.button} onPress={openModal}>
-            <Icon
-            name="help-circle"
-            style={styles.faq}
-            /> 
-            <Text>¿No se actualiza el saldo?</Text>
-          </TouchableOpacity>
-        </View>
-        
+        <View style={styles.containerFaq}>
 
-            <Icon
-            name="help-circle"
-            style={styles.faq}
-            /> 
-            <Text>¿La transferencia tarda más de lo esperado?</Text>
-         
-            <Icon
-            name="help-circle"
-            style={styles.faq}
-            /> 
-            <Text>¿Por qué fallo mi retiro?</Text>
-        
+          <View style={styles.question}>
+            <TouchableOpacity style={styles.button} onPress={openModal}>
+              <Icon
+              name="help-circle"
+              style={styles.faq}
+              /> 
+              <Text style={styles.textFaq}>¿No se actualiza el saldo?</Text>
+            </TouchableOpacity>
+            <View style={styles.divider}></View>
+          </View>
+
+          <View style={styles.question}>
+            <TouchableOpacity style={styles.button} onPress={openModal}>
+              <Icon
+              name="help-circle"
+              style={styles.faq}
+              /> 
+              <Text style={styles.textFaq}>¿La transferencia tarda más de lo esperado?</Text>
+            </TouchableOpacity>
+            <View style={styles.divider}></View>
+          </View>
+          
+          <View style={styles.question}>
+            <TouchableOpacity style={styles.button} onPress={openModal}>
+              <Icon
+              name="help-circle"
+              style={styles.faq}
+              /> 
+              <Text style={styles.textFaq}>¿No se pudo realizar tu retiro de dinero?</Text>
+            </TouchableOpacity>
+            <View style={styles.divider}></View>
+          </View> 
+
+        </View>
+
+        <View style={styles.chatboxContainer}>
+          <Icon
+          name="chat-processing"
+          style={styles.chatbox}/>
+        </View>
+      
       </View>
 
     );
@@ -55,25 +75,70 @@ export default function Form() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#0000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  
-    faq:{
-      fontSize:30
+      backgroundColor: '#00C4B3',
+      justifyContent: 'flex-end',
+      width:'100%' ,
     },
 
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        paddingHorizontal: 10
-      },
-      button: {
-        alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 10
-      }
+    containerFaq: {
+      flex: 0.3,
+      // justifyContent: 'center',
+      justifyContent: 'space-evenly',
+      // justifyContent:'space-around',
+      backgroundColor: '#F7F7F7',
+      borderRadius: 8,
+      marginLeft:16,
+      marginRight: 16,
+      marginBottom: 22,
+      paddingTop: 8,
+      paddingBottom: 8,
+      // borderTopWidth: 16
+    },
+
+    question: {
+      marginLeft:16,
+      marginRight: 16,
+      marginTop: 16,
+    },
+    
+    button: {
+      flexDirection: "row", 
+      paddingRight: 15 
+    },
+  
+    faq: {
+      // position: 'relative',
+      fontSize: 25,
+      color: '#00BAA4',
+      marginRight: 8
+    },
+
+    textFaq: {
+      fontSize: 16,
+      color: '#595959',
+      marginBottom: 14,
+      textAlign: 'left',
+    },
+
+    divider: {
+      width:'100%',
+      height:1,
+      backgroundColor: '#D6D6D6',
+      opacity: 5
+
+    },
+
+    chatboxContainer: {
+      backgroundColor: '#595959'
+
+    },
+
+    chatbox: {
+      fontSize:30,
+      marginBottom: 44
+
+    }
+     
   });
 
   
