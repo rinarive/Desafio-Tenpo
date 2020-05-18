849e28c177f00adb133bc2a18f322c3518a8369b
Consuelo W <consue_1990@hotmail.com>
2020-05-18 10:28:32 -0400

    Componente Form Modal (#2)
    * instalacion EXPO para react native
    
    * creando componente seccion preguntas y componente Modal
    
    * caja de preguntas estilos OK
    
    * FAQ y Modal OK
    
    * modifcando y editando package-lock.json
    
    * merge con rama developer de Rina

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Home from './components/Home'
+import { StyleSheet, Text, View } from 'react-native';
+import Form from './Components/Form';
+// import Home from './components/Home'
 
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 
 
 export default function App() {
-
-
-  const [fontsLoaded] = useFonts({    
+    const [fontsLoaded] = useFonts({    
 
     'Gotham-Bold' : require('./assets/Fonts/Gotham-Bold.ttf'),
     'Gotham-Book' : require('./assets/Fonts/Gotham-Book.ttf'),
@@ -23,8 +23,16 @@ export default function App() {
     return  (
       <>
       <Home />  
+      <Form/>
       
       </>
     )
   }
 }
+
+
+
+
+
+
+
